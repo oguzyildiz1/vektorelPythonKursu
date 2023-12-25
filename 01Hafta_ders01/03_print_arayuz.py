6b85d0dbecd6f79f1f8ce7f4bcd19fe30fb08adf
oguzyildiz1 <150700256+oguzyildiz1@users.noreply.github.com>
2023-12-25 21:27:59 +0300

    ders01Hafta1

---
+++ 01Hafta_ders01/03_print_arayuz.py
@@ -0,0 +1,37 @@
+2+3
+print(2+3)
+print((8+2)*10)
+
+print("Merhaba")
+print("Nasılsın?")
+print("'Nasılsın='")
+print('''Eren'in annesi "gel" dedi''')
+print('erdincdonmez.com')
+
+print('Erdinç DÖNMEZ\n9C\n439') #metinsel ifadeler
+print('\tGoogle chrome')
+
+print('Mustafa\tDÖNMEZ')
+print('Mustafa\tAKA')
+print('Mustafa\tKARA')
+
+##print('Erdinç', 'DÖNMEZ')
+##print('Erdinç'+'DÖNMEZ')
+
+print("Erdinç" * 10)
+
+a = "Erdinç"
+
+print(a)
+print(a*len(a))
+
+
+print(len("Bilişim"))
+
+aaa = "Ders "
+print("Oğuzhan ")
+print(aaa * len(aaa))
+
+print("═════════════")
+
+input()
