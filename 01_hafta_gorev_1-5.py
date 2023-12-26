f7826e7002d627fce0823e7f1a63472f4eb45bfe
oguzyildiz1 <150700256+oguzyildiz1@users.noreply.github.com>
2023-12-26 15:03:22 +0300

    1. Hafta görevleri (1 ve 5 arasındakiler)

---
+++ 01_hafta_gorev_1-5.py
@@ -0,0 +1,20 @@
+print("Merhaba")
+
+print("Oguzhan Yildiz\n")
+print("═"*10 + " Sinifi " + "═"*10)
+print("Oguzhan Yildiz\n11. sinif\n732\n")
+
+print("═"*10 + " Istiklal Marsi " + "═"*10)
+print("Korkma sönmez bu safaklarda yüzen al sancak;")
+print("Sönmeden yurdumun üstünde tüten en son ocak,")
+print("Kahramandir o benim milletimin yildizidir, parlayacak;")
+print("O benimdir, o benim milletimindir ancak.\n")
+
+print("*"*7, "Ögrenci Bilgileri", "*"*7)
+print("\tAdi\t: ", "Oguzhan")
+print("\tSoyadi\t: ", "YILDIZ")
+print("\tNumarasi: ", 732)
+print("*"*33)
+
+input()
+
