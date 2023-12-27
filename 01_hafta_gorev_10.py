ddb9a3072fa2b771fed8420fcbfbedfa61f8716f
oguzyildiz1 <150700256+oguzyildiz1@users.noreply.github.com>
2023-12-27 16:30:10 +0300

    1.hafta 10. görev(kendi programım)

---
+++ 01_hafta_gorev_10.py
@@ -0,0 +1,12 @@
+## Hava durumu programı
+
+print("╔" + "═"*25 +"╗")
+print("║\tHava Durumu\t  ║")
+print("║   1. Şehir\t\t  ║")
+print("║   2. Gündüz\t\t  ║")
+print("║   3. Gece\t\t  ║")
+print("║   4. Sıcaklık çevirme   ║")
+print("╠"+ "═"*25 + "╣")
+print("║Seçiminiz: \t\t  ║")
+print("╚" + "═"*25 +"╝")
+input()
