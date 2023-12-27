14cb5c542a0cb17e7156d72c290fb9b291858a86
oguzyildiz1 <150700256+oguzyildiz1@users.noreply.github.com>
2023-12-27 16:29:28 +0300

    1.Hafta 6-9 arasındaki görevler

---
+++ 01_hafta_gorevler_6-9.py
@@ -0,0 +1,22 @@
+#### 6. görev: Diyalog
+print("═" * 7 + " 6. görev: Diyalog " + "═" * 7 +"\n")
+print("Mehmet:\t" + "Merhaba Gökhan. Bugün nasılsın?")
+print("Gökhan:\t" + "İyiyim teşekkür ederim Mehmet. Sen nasılsın?")
+print("Mehmet:\t" + "Bende iyiyim teşekkür ederim.","Bugün sinemaya gitmek ister misin?")
+print("Gökhan\t" + "Tabi ki. Hangi filme gidelim?")
+print("Mehmet:\t" + "Yeni film, Terminatör 5 gelmiş. Bu akşam gidebiliriz.")
+print("Gökhan\t" + "Tamam gidelim.")
+print("Mehmet\t" + "Tamam akşama görüşürüz.")
+
+## 7. görev
+print("\n"+"═" * 7 + " 7. görev " + "═" * 7)
+print("Ali'nin annesi gel dedi.")
+
+## 8. görev
+print("\n"+"═" * 7 + " 8. görev: Adı 100 kere yazdırma " + "═" * 7 +"\n")
+print("Oğuzhan " * 100)
+
+## 9. görev
+print("\n"+"═" * 7 + " 9. görev: Adı 10 kere alt alta yazdırma " + "═" * 7 +"\n")
+print("Oğuzhan\n" * 10)
+input()
