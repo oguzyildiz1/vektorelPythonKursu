7277f2d8313ecddaf849852b0e94f50981111785
oguzyildiz1 <yildizoguz1@gmail.com>
2024-01-08 20:57:36 +0300

    degisiklik

--- github_dersi/github_yukleme_adimlari.py
+++ github_dersi/github_yukleme_adimlari.py
@@ -1,3 +1,8 @@
+#dosya açma
+#dosya klasorunun içinde cmd yaz
+#cmd ekranında .code yaz (vscode ile açar)
+
+#dosya yükleme
 #dosya save et
 #git pull   // ile git'te değişmiş bir dosya var mı diye kontrol et
 #git add .
