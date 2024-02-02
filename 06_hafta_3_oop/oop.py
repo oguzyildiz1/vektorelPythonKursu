5af1b924c3a584aa98bf28bc62046d2b9b9de45e
Oğuzhan Yıldız <150700256+oguzyildiz1@users.noreply.github.com>
2024-02-02 20:31:19 +0300

    Add files via upload

---
+++ 06_hafta_3_oop/oop.py
@@ -0,0 +1,57 @@
+class Ilan():
+    def  __init__(self, n, b, s, a):
+        self.ilanNo = n
+        self.ilanBaslik = b
+        self.ilanSahibi = s
+        self.ilanAciklama = a
+        
+    def bilgi(self):
+        print("No:", self.ilanNo, "Başlık:", self.ilanBaslik)
+
+    def bilgi1(self):
+        return "No: " + self.ilanNo + "Başlık: ", self.ilanBaslik
+
+
+    def kaydet(self):
+        d= open("kayit.txt","w")
+        kb = f"{self.ilanNo} {self.ilanBaslik} {self.ilanSahibi}\n"
+        d.write(kb)
+        d.close()
+        self.bilgi()
+
+
+class Ev(Ilan):
+    def __init__(self, n, b, s, a , o= "--"):
+        self.odaSayisi = o
+    def bilgi(self):
+        print("Oda sayısı: ", self.odaSayisi, "Açıklaması: ", )
+
+
+class Arac(Ilan):
+    aracMarkasi = ""
+    def __init__(self, n, b, s, a, m, f):
+        self.aracMarkasi = a
+        self.__fiyat = f
+
+    def bilgi(self):
+        print("Marka: ", self.aracMarkasi, "Fiyatı: ", self.__fiyat)
+
+    def fiyatAyarla(self, abc):
+        self.__fiyat = abc
+
+
+ilan1 = Ilan(123, "Ev", "Ahmet Kara", "Evi hemen satmam gerektiği için bu işi halletttim.")
+
+ilan2 = Ev(143, "Ev", "Tarık Sorar", "Kiralık ev hemen teslim.", 3)
+ilan3 = Arac(1233, "Araba","Mustafa Korkmaz","Açıklama","Volvo", 123123)
+
+ilan1.bilgi()
+
+ilan2.bilgi()
+ilan3.bilgi()
+ilan2.odaSayisi = 5
+ilan3.aracMarkasi = "Toyota"
+ilan3.__fiyat = 565555
+ilan3.fiyatAyarla(569999)
+
+ilan3.bilgi()
